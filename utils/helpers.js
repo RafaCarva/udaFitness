b3eebfac9339581dd9c1c853468d314048bccbc7
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2018-01-29 23:26:34 -0200

    arrumar

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View,StyleSheet  } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { black } from './colors'
+import { red, orange, blue, lightPurp, pink, white } from './colors'
 
+const styles = StyleSheet.create({
+    iconContainer: {
+      padding: 5,
+      borderRadius: 8,
+      width: 50,
+      height: 50,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginRight: 20
+    },
+  })
 
 export function getDailyReminderValue() {
   return {
@@ -20,7 +31,7 @@ export function getMetricMetaInfo(metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: red}]}>
             <MaterialIcons
               name='directions-run'
               color={black}
@@ -38,7 +49,7 @@ export function getMetricMetaInfo(metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
             <MaterialCommunityIcons
               name='bike'
               color={black}
@@ -56,7 +67,7 @@ export function getMetricMetaInfo(metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
             <MaterialCommunityIcons
               name='swim'
               color={black}
@@ -74,7 +85,7 @@ export function getMetricMetaInfo(metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
             <FontAwesome
               name='bed'
               color={black}
@@ -92,7 +103,7 @@ export function getMetricMetaInfo(metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
+          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
             <MaterialCommunityIcons
               name='food'
               color={black}
